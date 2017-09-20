59c052517c781b494868f88c2f3d948a37fc20d7
Drew Botka <drewbotka@gmail.com>
2017-09-20 11:26:33 -0400

    db manager class added

--- src/main/java/io/drewbot/Main.java
+++ src/main/java/io/drewbot/Main.java
@@ -1,30 +1,29 @@
 package io.drewbot;
 
+import io.drewbot.Helpers.DatabaseManager;
+import io.drewbot.models.Stat;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
 
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
-            // CREATE TABLE stats { id INTEGER PRIMARY KEY, name STRING, wins INTEGER, losses INTEGER }
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("DROP TABLE IF Exists stats;");
-            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY KEY, name STRING, wins INTEGER, losses INTEGER);");
-            statement.executeUpdate("INSERT INTO stats (name, wins, losses) VALUES ('drew', 3, 1);");
-            ResultSet rs = statement.executeQuery("SELECT * FROM stats;");
+
+            DatabaseManager dbm = new DatabaseManager(conn);
+
+            dbm.dropStatTable();
+
+            dbm.createStatTable();
 
             System.out.println("stats database created");
 
-            // While there are result sets next
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int wins = rs.getInt("wins");
-                int losses = rs.getInt("losses");
+            dbm.insertIntoStatTable("Floyd", 50, 0);
+            dbm.insertIntoStatTable("Jimmy", 4, 3);
 
-                System.out.printf("id: %s, %s %s %s\n", id, name, wins, losses);
-            }
+            ArrayList<Stat> results = dbm.getStats();
 
         } catch (SQLException e) {
             System.out.println("We encountered a problem connecting to the database");
---
+++ src/main/java/io/drewbot/models/Stat.java
@@ -0,0 +1,16 @@
+package io.drewbot.models;
+
+public class Stat {
+
+    private int id;
+    private String name;
+    private int wins;
+    private int losses;
+
+    public Stat(int id, String name, int wins, int losses) {
+        this.id = id;
+        this.name = name;
+        this.wins = wins;
+        this.losses = losses;
+    }
+}
